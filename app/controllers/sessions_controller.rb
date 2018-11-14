b787cfad9001dce1a0d329c7824c3e86ce3f8eba
Joshua <joshuaprpich@gmail.com>
2018-11-14 10:54:08 -0500

    implement login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      render plain: "super lo lograste"
+      log_in user
+      redirect_to '/'
     else
-      render plain: "no lo lograste"
+      render 'new'
     end
   end
 
+  def destroy
+    log_out
+    redirect_to '/'
+  end
+  
 end
