0a806923047b5c58c4dfc8050481135b03a5d0dd
Joshua <joshuaprpich@gmail.com>
2018-11-14 11:01:49 -0500

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   get '/', to: 'users#index'
 
+  # auth routes
   get  '/signup',  to: 'users#new', as: 'signup'
   post '/signup',  to: 'users#create'
-
-  get    '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new', as: 'login'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
 
-  
+  # pages routes
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/team', to: 'pages#team'
+
 end
